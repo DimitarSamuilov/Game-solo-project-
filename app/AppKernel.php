e5030599b5ac5570e9ebdf23d0713d82fc73f99b
DimitarSamuilov <dimsamuilov@abv.bg>
2016-12-17 10:37:21 +0200

    Final result 17.12.2016

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ArchBundle\ArchBundle(),
+            new JMose\CommandSchedulerBundle\JMoseCommandSchedulerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
